8d9c98c773716dc2e548c6dfc35202321efd75b0
Clay Smith <casmith@users.noreply.github.com>
2022-02-20 23:48:06 -0800

    Convert habitica.js to async/await

--- habitica.js
+++ habitica.js
@@ -19,74 +19,81 @@ module.exports = class Habitica {
         return new Habitica(newAxios, logger);
     }
 
-    post(url, form) {
-        return this.axios.post(url, form)
-            .then(res => res.data);
+    async post(url, form) {
+        const res = await this.axios.post(url, form)
+        return res.data;
     }
 
-    get(url) {
-        return this.axios.get(url)
-            .then(response => response.data);
+    async get(url) {
+        const response = await this.axios.get(url)
+        return response.data; 
     }
 
-    createTask (task) {
-        return this.post(`/tasks/user`, task);
+    async createTask (task) {
+        return await this.post(`/tasks/user`, task);
     }
 
-    getTask(taskId) {
-        return this.get(`/tasks/${taskId}`);
+    async getTask(taskId) {
+        return await this.get(`/tasks/${taskId}`);
     }
 
-    updateTask (task) {
-        return this.axios.put(`/tasks/${task.alias}`, task);
+    async updateTask (task) {
+        return await this.axios.put(`/tasks/${task.alias}`, task);
     }
 
-    deleteTask (taskId) {
-        return this.axios.delete(`/tasks/${taskId}`).catch(err => {
+    async deleteTask (taskId) {
+	try {
+	    return await this.axios.delete(`/tasks/${taskId}`)
+	} catch (err) {
             if (err.response.status === 404) {
                 this.logger.warn('Deleting task that no longer exists', taskId);
             } else {
-                return Promise.reject(err);
+                throw err;
             }
-        });
+        }
     }
 
-    deleteAllTasks(type) {
-        return this.listTasks(type)
-            .then(tasks => Promise.all(tasks.map(t => this.deleteTask(t._id))));
+    async deleteAllTasks(type) {
+        const tasks = await this.listTasks(type);
+        await deleteTasks(tasks.map(task => task._id));
     }
 
-    deleteTasks (taskIds) {
-        return Promise.all(taskIds.map(taskId => this.deleteTask(taskId)));
+    async deleteTasks (taskIds) {
+        for (const taskId of taskIds) {
+            await this.deleteTask(taskId);
+        }
     }
 
-    listTasks(type = 'todos') {
-        return this.get('/tasks/user' + (type ? `?type=${type}` : ''))
-            .then(response => response.data);
+    async listTasks(type = 'todos') {
+       const response = await this.get('/tasks/user' + (type ? `?type=${type}` : ''))
+       return response.data;     
     }
 
-    listDailies() {
-        return this.listTasks('dailys');
+    async listDailies() {
+        return await this.listTasks('dailys');
     }
 
-    scoreTask (taskId) {
-        return this.post(`/tasks/${taskId}/score/up`);
+    async scoreTask (taskId) {
+        return await this.post(`/tasks/${taskId}/score/up`);
     }
 
-    createChecklistItem(taskId, text) {
-        return this.post(`/tasks/${taskId}/checklist`, text);
+    async createChecklistItem(taskId, text) {
+        return await this.post(`/tasks/${taskId}/checklist`, text);
     }
 
-    updateChecklistItem(taskId, itemId, text) {
-        return this.axios.put(`/tasks/${taskId}/checklist/${itemId}`, {text});
+    async updateChecklistItem(taskId, itemId, text) {
+        return await this.axios.put(`/tasks/${taskId}/checklist/${itemId}`, {text});
     }
 
-    deleteChecklistItem(taskId, itemId) {
-        return this.axios.delete(`/tasks/${taskId}/checklist/${itemId}`);
+    async deleteChecklistItem(taskId, itemId) {
+        return await this.axios.delete(`/tasks/${taskId}/checklist/${itemId}`);
     }
 
-    scoreChecklistItem(taskId, itemId) {
-        return this.post(`/tasks/${taskId}/checklist/${itemId}/score`)
-            .catch(e => Promise.reject("failed to score up a task"));
+    async scoreChecklistItem(taskId, itemId) {
+        try {
+	    await this.post(`/tasks/${taskId}/checklist/${itemId}/score`);
+	} catch (e) {
+            throw new Error("failed to score up a task");
+        }
     }
 }
